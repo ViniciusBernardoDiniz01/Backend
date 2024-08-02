2531619786308ff5e268762901ab03038380da22
ViniciusBernardoDiniz01 <vinicius.diniz.senai404@gmail.com>
2024-08-02 09:50:08 -0300

    Add files via upload

---
+++ PHP/index.php
@@ -0,0 +1,17 @@
+<?php
+// Chama as variaveis
+$nome = 'Vinicius';
+$escola = 'Sesi-sp';
+$CE = 428;
+$curso = 'Desenvolvimento de sistema';
+$media = 9.5;
+$saudacao = "Bem-vindo $nome, a escola $escola," ;
+
+// printa as variaveis na pagina web
+echo $nome . '<br>';
+echo $saudacao . '<br>';
+echo $escola . '<br>';
+echo $CE . '<br>';
+echo $curso . '<br>';
+echo $media . '<br>';
+?>
